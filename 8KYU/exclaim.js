fe31f4cccb085534c85be11f5896017fe334df22
bruh.ngl0 <adityashrm500@gmail.com>
2023-06-11 15:34:34 +0530

    good

--- 8KYU/exclaim.js
+++ 8KYU/exclaim.js
@@ -20,3 +20,37 @@
   }
   
   stringToArray("I love arrays they are my favorite")*/
+
+  /*function isIsogram(str){
+    let count = 0;
+    let str1 = str.toLowerCase()
+    let arr = str1.split('')
+    
+     arr.sort()
+     for(let i = 0 ; i < arr.length; i++){
+      if(arr[i] == arr[i+1]){
+         count+= 1
+      }  
+    }
+
+    console.log (count >= 1 ? false :true)
+
+    }
+    isIsogram("moOse") */
+
+
+  /*function isIsogram(str){
+
+      let count = 0;
+      
+      let arr = str.toLowerCase().split('')
+      arr.sort()
+       for(let i = 0 ; i < arr.length; i++){
+        if(arr[i] == arr[i+1]){
+           count+= 1
+        }  
+      }
+  
+      return (count >= 1 ? false :true)
+  
+      }*/
