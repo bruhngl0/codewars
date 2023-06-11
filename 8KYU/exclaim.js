401731aba44f480bc65876cb92e90a4a0236422f
bruh.ngl0 <adityashrm500@gmail.com>
2023-06-11 14:19:06 +0530

    8kyu

---
+++ 8KYU/exclaim.js
@@ -0,0 +1,22 @@
+/*function removeExclamationMarks(s) {
+    console.log(s.split("!").join(""))
+  }
+
+  removeExclamationMarks("he!llo!") */
+
+
+  /*function check(a, x) {
+    let arr = a.find((e)=> (e === x) )
+    console.log(arr? true:false)
+  }
+
+  check([], ) */
+
+  /*function stringToArray(string){
+
+    let arr = string.split(" ")
+    return arr
+
+  }
+  
+  stringToArray("I love arrays they are my favorite")*/
