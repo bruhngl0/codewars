ae60a61ae7c3820364cd777781e1ae1f8ed5a658
bruh.ngl0 <adityashrm500@gmail.com>
2023-06-13 21:50:27 +0530

    new

--- 8KYU/exclaim.js
+++ 8KYU/exclaim.js
@@ -206,3 +206,32 @@ var isSquare = function(n){
   return Math.sqrt(n) % 1 === '0'
 } */
 
+/*Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+For example:
+
+uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+uniqueInOrder([1,2,2,3,3])       == [1,2,3] */
+
+
+/*var uniqueInOrder=function(iterable){
+  let arr = []
+  
+  for(let i=0; i< iterable.length; i++){
+    if(iterable[i] != iterable[i+1]){
+      arr.push(iterable[i])
+    }
+  }
+   return arr
+ } */
+ 
+ /*Best Practice
+ const uniqueInOrder = d => [...d].filter((x, i, a) => x != a[i + 1]) */
+
+ /*function l(d){
+
+  console.log([...d])
+ } 
+
+l("abvxfg is a dog") */
